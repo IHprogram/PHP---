94676db95a3d106672fcfd0e59e75a51f76745fd
IHprogram <ihm20202020@gmail.com>
2020-12-23 13:51:05 +0900

    取得先のデータベースと取得

--- TASK02_on_chapter3/list.php
+++ TASK02_on_chapter3/list.php
@@ -1,15 +1,26 @@
+<!-- URL: http://localhost:8888/ServerSideCurriculum/TASK02_on_chapter3/list.php -->
+
 <!-- ここから、ページに表示するフロントです -->
 <!DOCTYPE html>
 <html>
- 
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="CSS/reset.css">
-    <link rel="stylesheet" href="CSS/style.css">
-    <title>3章CRUD実装-課題-</title>
-</head>
- 
-<body>
- 
-</body>
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="CSS/reset.css">
+        <link rel="stylesheet" href="CSS/style.css">
+        <link rel="stylesheet" href="CSS/responsive.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- fontawesome -->
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <!-- JQuery -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="js/base.js"></script>
+
+        <title>3章CRUD実装-課題-</title>
+    </head>
+
+    <body>
+
+    </body>
+
 </html>
