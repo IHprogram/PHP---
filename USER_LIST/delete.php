02c3adff3c18f425a51add2c338800dd92123c38
IHprogram <ihm20202020@gmail.com>
2020-12-28 19:40:16 +0900

    プリペアドステートメントに関する補足説明の追加

--- USER_LIST/delete.php
+++ USER_LIST/delete.php
@@ -8,7 +8,11 @@ $id = intval($_GET["id"]);
 try {
   // SQLインジェクションを防ぐためのプリペアドステートメント（という対策）
   $sql = "DELETE FROM users WHERE id = :id";
+  // 「引数に入れたSQL文をプリペアードステートメント対策で実行する」という設定
   $stmt = $pdo->prepare($sql);
+  // bindValueは、プリペアードステートメント形式で行うSQL文の中にある、
+  // この「:id」の部分に、なんの値が入るかを指定する役割を持っている
+  //そして、その値をただの文字列として認識させる（SQL文ではなく、ただのテキストにしてくれる）
   $stmt->bindValue(":id", $id, PDO::PARAM_INT);
   // ↓MySQLへの命令部分
   $stmt->execute();
