2e2af0b90c2832797763328a477f0a1ebb20df68
IHprogram <ihm20202020@gmail.com>
2020-12-29 22:42:31 +0900

    finalをプロパティやメソッドに使用した時の説明を追加

--- object/extend.php
+++ object/extend.php
@@ -9,18 +9,32 @@ class User{
         echo $this->name . 'はUserクラスからきてます';
         echo '<br>';
     }
+
+    // ↓finalを追加すれば、finalを追加したプロパティやメソッドを継承しないように設定できる。
+    // final public function hello(){
+    //     echo $this->name.'はUserクラスからきてます';
+    //     echo '<br>';
+    // }
 }
 
 // 子クラス
 class Admin extends User{
     public function hi(){
         echo '<p>下のテキストは、Adminクラスから来ています。</p>';
-        echo $this->hello();
+        echo $this->hello(); //Userのhelloメソッドにfinalを使っていた場合、「$this->hello( );」は利用することができなくなる。
         echo '<br>';
     }
 }
 
-// インスタンス化
+class User1 extends User{
+    public function yeah(){
+        echo '<h2>以下はyeahメソッドの処理結果です！</h2>';
+        echo $this->hello(); //Userのhelloメソッドにfinalを使っていた場合、「$this->hello( );」は利用することができなくなる。
+        echo '<br>';
+    }
+}
+
+// インスタンスの作成
 $user = new User();
 $admin = new Admin();
 
@@ -28,3 +42,4 @@ echo $user->name . '<br>'; //表示結果 → DAWN太郎
 $user->hello(); // 表示結果 → DAWN太郎はUserクラスからきてます
 $admin->hello(); // 表示結果 → DAWN太郎はUserクラスからきてます
 $admin->hi(); // 表示結果 → 下のテキストは、Adminクラスから来ています。 （改行を挟んで） DAWN太郎はUserクラスからきてます
+?>
